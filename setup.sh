117f6aed1aaa9e18a3860664df4963a82312a8e8
B.Titze <77006072+Jodels2002@users.noreply.github.com>
2023-04-02 09:58:14 +0200

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# Install AmiRock
+# B.Titze 2023
+
+
+BLACK='\033[0;39m'
+BLUE='\033[1;34m'
+GREEN='\033[1;32m'
+RED='\033[1;31m'
+GREY='\033[1;30m'
+
+  sudo apt install -y  toilet 
+  clear
+  toilet "Rock5b OLED" --metal
+      echo " "
+      echo -e "$BLUE "
+      echo "installing now ... "
+      echo -e "$GREY " 
+      sudo apt install -y dialog mc zip unzip wget 7zip
+  
